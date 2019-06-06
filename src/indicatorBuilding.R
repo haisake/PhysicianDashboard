3ae8aaa7da18deb6d54150b270d291c67d205dec
haisake <HAisake@vch.ca>
2019-06-06 15:09:53 -0700

    data loading features
    stopped mid development to revisit design

--- src/indicatorBuilding.R
+++ src/indicatorBuilding.R
@@ -17,4 +17,27 @@ setwd(wd)
 
 #load libraries
 library(RODBC)
-library(ggplot2)
+
+#get query from a .sql file
+getQuery <-function(fileName){
+  rawQueryTxt <-readChar(fileName, file.info(fileName)$size)
+  finalQueryTxt <-gsub( "[\r\n\t]", " ", rawQueryTxt) #remove spaces, newlines, and tabs
+}
+
+#Purpose: load other data sets given a ODBC connection anme and queryfile name
+loadCensus <-function(connectionName, queryFileName)
+{
+  uid <- readline("User id: ")
+  pwd <- readline("Password: ")
+  channel <- odbcConnect(connectionName, uid, pwd)  #set the connection details
+  query <- getQuery(queryFileName)        #get the query string from the external query file
+  x <-sqlQuery(channel, query)          #query the database and bring the data in as a table
+  
+  odbcCloseAll() #close data base connection
+  return(x) #return the results
+}
+
+#censusQuery
+censusData          <- loadDataGeneric("CAPPLAN","censuQuery.sql")
+doctorServicesTable <- loadDataGeneric("AISAKE-DSSI","doctorServicesQuery.sql")
+
