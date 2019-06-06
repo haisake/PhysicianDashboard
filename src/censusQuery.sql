bb973e6ad294f73c94566a846d45aab1b9dc4483
haisake <HAisake@vch.ca>
2019-06-06 10:49:31 -0700

    Now computes census by specified hour for dates in a range

--- src/censusQuery.sql
+++ src/censusQuery.sql
@@ -1,41 +1,52 @@
-declare @censusdate datetime,@counter int,@countermax int
-set @censusdate=dateadd(dd,-(365+30+22),dateadd(mi,900,cast(cast(getdate() as date) as datetime)))
-set @counter=1
-set @countermax=365+30+22
+/*
+Purpose: To comptue the census by provider and nursing unit at Richmond hospital from CapPlan given a specified hour for a specified date range
+Author: Hans Aisake
+Date Created: June 6, 2019
+Date Modified:
+Comments:
+	The base idea came from Peter's query. I removed the classification as Hospitalist, GIM, etc... from this query.
+	Those linkages have to be found elsewhere in other databses like DSSI if anywhere at all.
+	Hard coding a mapping into this query was not desired, but we will have sufficient foundations to do it later
+ */
 
-while @counter<=@countermax
-begin
-insert into #census
-SELECT @censusdate 
-      ,'15:00' as censustime
-      ,[AssignmentID]
-      ,[EncounterID]
-      ,[AssignmentDate] as AdmitDate
-      ,[lu_WardID] as Unit
-      ,case when [lu_SpecialtyID] ='ALC' then 'ALC' else 'Acute' end as ALCFlag
-      ,d.DrService
-      ,[lu_HealthCareProfessionalID] as DrCode
-  ,isnull([lu_BedID],'UNKNOWN') as Bed
-   , case when [lu_HealthCareProfessionalID] in (select drcode COLLATE DATABASE_DEFAULT from #drservice where drservice='hospitalist') and [lu_WardID]='R4N' then [lu_HealthCareProfessionalID] + '^^R4N' else [lu_HealthCareProfessionalID] end as LookupDrCodeUnit
-FROM [CapPlan_RHS].[dbo].[Assignments] a
-left outer join #drservice d 
-on case when [lu_HealthCareProfessionalID] in (select drcode COLLATE DATABASE_DEFAULT from #drservice where drservice COLLATE DATABASE_DEFAULT ='hospitalist') and [lu_WardID] ='R4N' then [lu_HealthCareProfessionalID] + '^^R4N' else [lu_HealthCareProfessionalID] end=d.drcode COLLATE DATABASE_DEFAULT
-where assignmentdate<@censusdate
-and (assignmentenddate >= @censusdate or assignmentenddate is null)
-and lu_wardid not like 'm[0-9]%'
-and lu_wardid not in ('rhbcb','ramb')
+/*Parameters for the query*/
+DECLARE @CensusHour int = 7;
+DECLARE @start_date DATETIME = DATEADD(year, -1, DATEADD(hh, @CensusHour, DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0))); /*get the current date, set time to @CensusHour, and then reduce the year by 1*/
+DECLARE @end_date DATETIME = DATEADD(hh, @CensusHour, DATEADD(dd, DATEDIFF(dd, 0, GETDATE()), 0)); /*get the current date, set time to @CensusHour*/
 
-set @censusdate=dateadd(dd,1,@censusdate)
-set @counter=@counter+1
-end
+/*create a list of dates based on the parameters*/
+WITH AllDays AS 
+( SELECT @start_date AS [Date]
+  UNION ALL
+  SELECT DATEADD(DAY, 1, [Date])
+  FROM AllDays
+  WHERE [Date] < @end_date 
+)
 
-select * from #census
-
-IF OBJECT_ID('tempdb..#census2') IS NOT NULL DROP TABLE #census2
-select censusdate,count(*) as census 
-into #census2
-from #census
-where drservice='Internal Medicine'
-group by censusdate
-
-select * from #census2
+/*find the census for each provider on the dates*/
+SELECT @CensusHour as 'CensusHour'
+, case when [lu_SpecialtyID]='ALC' then 'ALC' 
+	   else 'Acute' 
+END as ALCFlag
+, [lu_HealthCareProfessionalID] as 'DrCode'
+, [lu_SpecialtyID]
+, [lu_WardID] as 'NursingUnitCode'
+, COUNT(1) as 'Census'
+/* these fields aren't required
+, [AssignmentID]
+, [EncounterID]
+, [AssignmentDate]	/*start A*/
+, [AssignmentEndDate] /*end E*/
+, isnull([lu_BedID],'UNKNOWN') as 'Bed'
+*/
+FROM [CapPlan_RHS].[dbo].[Assignments] as X
+INNER JOIN AllDays as Y
+ON Y.[Date] BETWEEN AssignmentDate AND ISNULL(AssignmentEndDate,'2050-01-01')	--filter to days relevant between @start and @end and assign a date for computing census
+where X.lu_wardid not like 'm[0-9]%'	/*ignore minoru*/
+and X.lu_wardid not in ('rhbcb','ramb') /*ignore birth center and ambulatory care*/
+GROUP BY case when [lu_SpecialtyID]='ALC' then 'ALC' 
+	   else 'Acute' 
+END
+, [lu_HealthCareProfessionalID]
+, [lu_SpecialtyID]
+, [lu_WardID]
