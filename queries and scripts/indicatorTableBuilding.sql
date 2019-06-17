b6e044ace4ef622f2a806270d6d2005c6e0aa870
Hans <hw.aisake@gmail.com>
2019-06-17 09:59:20 -0700

    added fiscal year dates

--- queries and scripts/indicatorTableBuilding.sql
+++ queries and scripts/indicatorTableBuilding.sql
@@ -26,17 +26,26 @@ List of Indicators
 */
 
 -----------------------------------
--- FP Table
+-- FP Table  dim.FPFY
 -----------------------------------
-IF OBJECT_ID('tempdb.dbo.#physDB_reportFP') is not NULL DROP TABLE #physDB_reportFP;
-GO
+	IF OBJECT_ID('tempdb.dbo.#physDB_reportFP') is not NULL DROP TABLE #physDB_reportFP;
+	GO
 
-SELECT distinct TOP 36  fiscalyear, fiscalperiodlong, fiscalperiodstartdate, fiscalperiodenddate
-INTO #physDB_reportFP
-FROM ADTCMart.dim.[Date]
-WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	--FP over for at least a year
-ORDER BY fiscalperiodlong desc
-;
+	WITH TEMP AS (
+		SELECT distinct TOP 36  fiscalyear, fiscalperiodlong, fiscalperiodstartdate, fiscalperiodenddate
+	
+		FROM ADTCMart.dim.[Date]
+		WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	--FP over for at least a year
+		ORDER BY fiscalperiodlong desc
+	)
+
+	--add on fiscal year dates
+	SELECT X.*, Y.FiscalYearStartDate, Y.FiscalYearEndDate
+	INTO #physDB_reportFP
+	FROM TEMP as X
+	LEFT JOIN (SELECT fiscalyear, min(fiscalperiodstartdate) as 'FiscalYearStartDate', max(fiscalperiodenddate) as 'FiscalYearEndDate' FROM TEMP GROUP BY fiscalyear) as Y
+	ON X.FiscalYear=Y.FiscalYear
+	;
 
 -----------------------------------
 -- service table
