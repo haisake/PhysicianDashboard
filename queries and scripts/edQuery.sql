3eccc2b9d634aba07b1556c25b00443186d51aea
Hans <hw.aisake@gmail.com>
2019-06-17 17:13:58 -0700

    converted queries to R friendly versions

--- queries and scripts/edQuery.sql
+++ queries and scripts/edQuery.sql
@@ -1,59 +1,39 @@
 /*
-Purpose: To build the indicator table for the physician dashboard
+Purpose: To pull required ED visit data for the physician dashboard indicators.
 Author: Hans Aisake
 Date Created: June 14, 2019
 Date Modified:
 Comments:
 
-List of Indicators
-	- Percentage of days where census ≤ target(or funded level) –  by service (CAPPLAN)
-	- Distribution of census by service is typical or atypical (CAPPLAN)
-	- Distribution of census by TOD is typical or atypical (CAPPLAN)
-	- # inpatient days (CAPPLAN)
-	- # of ALC days or ALC rate % (CAPPLAN)
-	- ALOS* days by service (CAPPLAN)
-	- LOS days distribution typical or atypical (CAPPLAN)
-	- ALOS/ELOS (ADRMart)
-	- ALOS/ELOS top 5 most significant* worst CMGs (ADRMart)
-	- Admission rate by service (EDMart)
-	- Admission Volumes By ToD & Service Distribution typical or atypical (EDMart)
-	- Discharge Volumes By ToD & Service Distribution typical or atypical (CAPPLAN)
-	- # ED consults by service (EDMART)
-	- LLOS days snapshot (CAPPLAN)
-	- LLOS days snapshot ALC (CAPPLAN)
-	- Service transfer volumes typical or atypical to other services (CAPPLAN)
-	- 7 and 28 day raw readmission rates (or volumes ) by services (CAPPLAN)
+Can't get consulting physician as it is not entered.
+Instead services are requested.
+
 */
 
------------------------------------
--- FP Table  dim.FPFY
------------------------------------
-	IF OBJECT_ID('tempdb.dbo.#physDB_reportFP') is not NULL DROP TABLE #physDB_reportFP;
-	GO
+/* fiscal period and year table */
+WITH fpDates AS (
+	SELECT distinct TOP 39  fiscalyear, fiscalperiodlong, fiscalperiodstartdate, fiscalperiodenddate
+	FROM ADTCMart.dim.[Date]
+	WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	--FP over for at least a year
+	ORDER BY fiscalperiodlong desc
+)
 
-	WITH TEMP AS (
-		SELECT distinct TOP 39  fiscalyear, fiscalperiodlong, fiscalperiodstartdate, fiscalperiodenddate
-	
-		FROM ADTCMart.dim.[Date]
-		WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	--FP over for at least a year
-		ORDER BY fiscalperiodlong desc
-	)
+, fyDates AS (
+	SELECT fiscalyear, min(fiscalperiodstartdate) as 'FiscalYearStartDate', max(fiscalperiodenddate) as 'FiscalYearEndDate' 
+	FROM fpDates
+	GROUP BY fiscalyear
+)
 
-	--add on fiscal year dates
+/* add on fiscal year dates */
+, fpFyDates AS (
 	SELECT X.*, Y.FiscalYearStartDate, Y.FiscalYearEndDate
-	INTO #physDB_reportFP
-	FROM TEMP as X
-	LEFT JOIN (SELECT fiscalyear, min(fiscalperiodstartdate) as 'FiscalYearStartDate', max(fiscalperiodenddate) as 'FiscalYearEndDate' FROM TEMP GROUP BY fiscalyear) as Y
+	FROM fpDates as X
+	LEFT JOIN fyDates as Y
 	ON X.FiscalYear=Y.FiscalYear
-	;
+)
 
--------------------------------------------
--- Pull ED data; can't get consulting physician ; dbo.ED
--------------------------------------------
-IF OBJECT_ID('tempdb.dbo.#ED') is not NULL DROP TABLE #ED;
-GO
-
-SELECT TOP 10 startdate
+/* Pull ED visit data. */
+SELECT startdate
 , visitID
 , AdmittedFlag
 , BedRequestDate as 'Admit_Date'
@@ -68,9 +48,9 @@ SELECT TOP 10 startdate
 	   ELSE 'Other'
 END as 'ConsultationServiceDesc'
 , InpatientNursingUnitName
-INTO #ED
 FROM EDMart.dbo.vwEDVisitIdentifiedRegional as ED
-INNER JOIN #physDB_reportFP as D
+INNER JOIN fpFyDates as D /* filter to fiscal periods of interest */
 ON ED.StartDate BETWEEN D.FiscalPeriodStartDate AND D.FiscalPeriodEndDate
 WHERE ED.FacilityShortName='RHS'
-;
+
+
