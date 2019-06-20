dddf595fbb91a73b64dada9c5f5faa8680ef5186
Hans <hw.aisake@gmail.com>
2019-06-20 14:00:29 -0700

    converted to a format easily run in R

--- queries and scripts/dadQuery.sql
+++ queries and scripts/dadQuery.sql
@@ -1,74 +1,48 @@
 /*
-Purpose: To build the indicator table for the physician dashboard
+Purpose: To comptue the pull required DAD records for the physician dashboar ALOS/ELOS computations
 Author: Hans Aisake
-Date Created: June 14, 2019
+Date Created: June 6, 2019
 Date Modified:
 Comments:
+	The base idea came from Peter's query. I removed the classification as Hospitalist, GIM, etc... from this query.
+	Those linkages have to be found elsewhere in other databses like DSSI if anywhere at all.
+ */
 
-List of Indicators
-	- Percentage of days where census ≤ target(or funded level) –  by service (CAPPLAN)
-	- Distribution of census by service is typical or atypical (CAPPLAN)
-	- Distribution of census by TOD is typical or atypical (CAPPLAN)
-	- # inpatient days (CAPPLAN)
-	- # of ALC days or ALC rate % (CAPPLAN)
-	- ALOS* days by service (CAPPLAN)
-	- LOS days distribution typical or atypical (CAPPLAN)
-	- ALOS/ELOS (ADRMart)
-	- ALOS/ELOS top 5 most significant* worst CMGs (ADRMart)
-	- Admission rate by service (EDMart)
-	- Admission Volumes By ToD & Service Distribution typical or atypical (EDMart)
-	- Discharge Volumes By ToD & Service Distribution typical or atypical (CAPPLAN)
-	- # ED consults by service (EDMART)
-	- LLOS days snapshot (CAPPLAN)
-	- LLOS days snapshot ALC (CAPPLAN)
-	- Service transfer volumes typical or atypical to other services (CAPPLAN)
-	- 7 and 28 day raw readmission rates (or volumes ) by services (CAPPLAN)
-*/
 
------------------------------------
--- FP Table  dim.FPFY
------------------------------------
-	IF OBJECT_ID('tempdb.dbo.#physDB_reportFP') is not NULL DROP TABLE #physDB_reportFP;
-	GO
 
 	WITH TEMP AS (
 		SELECT distinct TOP 39  fiscalyear, fiscalperiodlong, fiscalperiodstartdate, fiscalperiodenddate
-	
 		FROM ADTCMart.dim.[Date]
-		WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	--FP over for at least a year
+		WHERE FiscalPeriodEndDate < DATEADD(day, -1, GETDATE())	/* FP over for at least a year */
 		ORDER BY fiscalperiodlong desc
 	)
 
-	--add on fiscal year dates
-	SELECT X.*, Y.FiscalYearStartDate, Y.FiscalYearEndDate
-	INTO #physDB_reportFP
-	FROM TEMP as X
-	LEFT JOIN (SELECT fiscalyear, min(fiscalperiodstartdate) as 'FiscalYearStartDate', max(fiscalperiodenddate) as 'FiscalYearEndDate' FROM TEMP GROUP BY fiscalyear) as Y
-	ON X.FiscalYear=Y.FiscalYear
-	;
+	, physDB_reportFP AS (
+	/* add on fiscal year dates */
+		SELECT X.*, Y.FiscalYearStartDate, Y.FiscalYearEndDate
+		FROM TEMP as X
+		LEFT JOIN (SELECT fiscalyear, min(fiscalperiodstartdate) as 'FiscalYearStartDate', max(fiscalperiodenddate) as 'FiscalYearEndDate' FROM TEMP GROUP BY fiscalyear) as Y
+		ON X.FiscalYear=Y.FiscalYear
+	)
 
--------------------------------------------
--- Pull DAD data; dbo.DAD
--------------------------------------------
-IF OBJECT_ID('tempdb.dbo.#DAD') is not null DROP TABLE #DAD;
-GO
+	/*
+		Pull DAD data; dbo.DAD
+	*/
+	SELECT D.FiscalPeriodLong
+	, D.FiscalYear
+	, ADR.MostRespProviderCode
+	, CMGPlusCode +'-' + LEFT(CMGPlusDesc,40) as 'CMG'
+	, SUM([ELOS]) as 'Sum_ELOS'
+	, SUM([LOS]) as 'Sum_LOS'
+	, SUM(1) as 'NumCases'
+	FROM ADRMart.dbo.vwAbstractFact as ADR
+	INNER JOIN physDB_reportFP as D
+	ON CONVERT(datetime, CONVERT(varchar(8), ADR.DischargeDate),112) BETWEEN D.FiscalPeriodStartDate AND D.FiscalPeriodEndDate	/* between FP */
+	WHERE FacilityShortName='RHS'
+	AND  CMGStatusDesc  ='typical'
+	AND  CareType='A'
+	GROUP BY  D.FiscalPeriodLong
+	, D.FiscalYear
+	, ADR.MostRespProviderCode
+	, CMGPlusCode +'-' + LEFT(CMGPlusDesc,40)
 
-SELECT D.FiscalPeriodLong
-, D.FiscalYear
-, ADR.MostRespProviderCode
-, CMGPlusCode +'-' + LEFT(CMGPlusDesc,40) as 'CMG'
-, SUM([ELOS]) as 'Sum_ELOS'
-, SUM([LOS]) as 'Sum_LOS'
-, SUM(1) as 'NumCases'
-INTO #DAD
-FROM ADRMart.dbo.vwAbstractFact as ADR
-INNER JOIN #physDB_reportFP as D
-ON ADR.DischargeDate BETWEEN D.FiscalPeriodStartDate AND D.FiscalPeriodEndDate	--between FP
-WHERE FacilityShortName='RHS'
-AND  CMGStatusDesc  ='typical'
-AND  CareType='A'
-GROUP BY  D.FiscalPeriodLong
-, D.FiscalYear
-, CMGPlusCode +'-' + LEFT(CMGPlusDesc,40)
-;
-GO
