a3436ec7fbd3547ee7a0d1f9b4f4b566d58f3234
Hans <hw.aisake@gmail.com>
2019-07-02 11:05:21 -0700

    reorder opperations

--- queries and scripts/main.R
+++ queries and scripts/main.R
@@ -11,30 +11,24 @@
 wd <- "//vch.ca/departments/VCHDecisionSupport/Patient Flow/Richmond SSRS Reports/PhysicianDashboard/PhysicianDashboard/queries and scripts"
 setwd(wd)
 
-#set parameters
-dsdw_dsn <- "AISAKE-DSSI"
-capPlanConfig <- "H:/Hans/DSN Configs/CAPPLAN.yml"
-target_services <- c( "Internal Medicine", "Hospitalist" ) #targeted services data we want after joining
-queryFileList <- list.files()[   which( sapply(list.files(), function(x) grep(".sql",x) ) == 1 )  ] #all files with .sql in the folder
-
 #load in source files
 source("./R Source Files/intialization.R", local = TRUE)
 source("./R Source Files/dataLoading.R", local = TRUE)
-
 intialize() #intialize local variables and libraries
 
-dataList <- loadData(queryFileList, dsdw_dsn, capPlanConfig) # Pull in datasets
+#set parameters
+dsdw_dsn <- "AISAKE-DSSI"
+capPlanConfig <- "H:/Hans/DSN Configs/CAPPLAN.yml"
+queryFileList <- list.files()[   which( sapply(list.files(), function(x) grep(".sql",x) ) == 1 )  ] #all files with .sql in the folder
+
+#load in data
+dataList  <- loadData(queryFileList, dsdw_dsn, capPlanConfig) # Pull in datasets
 dataList2 <- dataList #temporary development var
+dataList  <- addFieldsAndAggregate( dataList ) #add things like service and aggregate the data when appropriate
 
-dataList <- addFieldsAndFilter( dataList )
+#compute the indicators
 
 
 
-####
-
-#at this point all the relevant data sets are made and now I need to compute the indicators
-#the start and end dates aren't utilized yet and would need to be changed in the join conditions above to be used properly
-census_df <- select(census_df,-c(lu_SpecialtyID, DrName, DS_StartDate, DS_EndDate )) 
-transfers_df <- select(census_df,-c(lu_SpecialtyID, DrName, DS_StartDate, DS_EndDate )) 
 
 
